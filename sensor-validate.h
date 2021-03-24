aa1229c713af93400e030a092f111f926e0fb5e3
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-03-25 00:39:48 +0530

    Update sensor-validate.h
    Function name changed and commented functions removed

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,8 +1,4 @@
 
-int _give_me_a_good_name(double value, double nextValue, double maxDelta);
-
-//int validateSOCreadings(double* values, int numOfValues);
-
-//int validateCurrentreadings(double* values, int numOfValues);
+int detectjumpsandreact(double value, double nextValue, double maxDelta);
 
 int validatereadings(double* values, int numOfValues, double limit);
