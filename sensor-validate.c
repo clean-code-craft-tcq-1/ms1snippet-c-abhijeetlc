f0ad3d3fe6b4124ea3441d60d07fe678892e4ae4
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-03-25 01:06:23 +0530

    Update sensor-validate.c
    comments added

--- sensor-validate.c
+++ sensor-validate.c
@@ -1,5 +1,10 @@
-#include "sensor-validate.h"
+/*-------------------------Source File ----------------------------------*/
+/*Author : Abhijeet C */
+#include "sensor-validate.h" /*Header Includes*/
 
+/* Funtion to detect jumps between 2 values*/
+/*Arguements : current value, next value , maxdelta */
+/*return type : integer*/
 int detectjumpsandreact(double value, double nextValue, double maxDelta) {
   if(nextValue - value > maxDelta) {
     return 0;
@@ -7,7 +12,9 @@ int detectjumpsandreact(double value, double nextValue, double maxDelta) {
   return 1;
 }
 
-
+/* Funtion to validate the sensor readings */
+/*Arguements : pointer to readings, number of values , maxdelta value */
+/*return type : integer*/
 int validatereadings(double* values, int numOfValues, double limit) {
 
   for(int index = 0; index < numOfValues; index++) {
