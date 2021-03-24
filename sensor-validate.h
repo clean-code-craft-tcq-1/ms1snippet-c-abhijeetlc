049775dacfe5f8a8ef0ff56a6cbbbca697aa0ccd
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-03-24 23:04:39 +0530

    Update sensor-validate.h
    commented SOC and Current functions

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,8 +1,8 @@
 
 int _give_me_a_good_name(double value, double nextValue, double maxDelta);
 
-int validateSOCreadings(double* values, int numOfValues);
+//int validateSOCreadings(double* values, int numOfValues);
 
-int validateCurrentreadings(double* values, int numOfValues);
+//int validateCurrentreadings(double* values, int numOfValues);
 
 int validatereadings(double* values, int numOfValues, double limit);
