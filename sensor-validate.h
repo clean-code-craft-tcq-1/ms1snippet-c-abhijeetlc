491400a0cd5c77f60af9c6e54570f23d43fa399e
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-03-25 00:50:03 +0530

    Update sensor-validate.h
    Comments added

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,4 +1,6 @@
+/*-------------------------Header-----------------------*/
+/* Author : Abhijeet C */ 
+/* Header includes the function prototype for funtions that detect jumps between values and validates the readings respectively*/
 
 int detectjumpsandreact(double value, double nextValue, double maxDelta);
-
 int validatereadings(double* values, int numOfValues, double limit);
