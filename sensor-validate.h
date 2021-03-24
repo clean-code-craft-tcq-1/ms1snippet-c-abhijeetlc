b6a600f9cb5431dd4d8d24636f712e0e35e43293
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-03-24 22:50:35 +0530

    Update sensor-validate.h
    Added common function prototype

--- sensor-validate.h
+++ sensor-validate.h
@@ -4,3 +4,5 @@ int _give_me_a_good_name(double value, double nextValue, double maxDelta);
 int validateSOCreadings(double* values, int numOfValues);
 
 int validateCurrentreadings(double* values, int numOfValues);
+
+int validatereadings(double* values, int numOfValues, double limit);
